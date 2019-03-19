cd709485ae1080f0d92bc65111bd932fa26731d3
Matheus Del Claro <32961172+mdelclaro@users.noreply.github.com>
2019-03-19 15:12:37 -0300

    Add files via upload

---
+++ RegisterScreens.js
@@ -0,0 +1,47 @@
+import { Navigation } from "react-native-navigation";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import Auth from "./screens/Auth";
+import SideMenu from "./screens/SideMenu";
+import Main from "./screens/Main";
+
+import { store, persistor } from "./store/configureStore";
+
+const registerScreens = () => {
+  Navigation.registerComponent(
+    "motoapp.Main",
+    () => () => (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Main />
+        </PersistGate>
+      </Provider>
+    ),
+    () => Main
+  );
+  Navigation.registerComponent(
+    "motoapp.Auth",
+    () => () => (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Auth />
+        </PersistGate>
+      </Provider>
+    ),
+    () => Auth
+  );
+  Navigation.registerComponent(
+    "motoapp.SideMenu",
+    () => () => (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <SideMenu />
+        </PersistGate>
+      </Provider>
+    ),
+    () => SideMenu
+  );
+};
+
+export default registerScreens;
